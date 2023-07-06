8b54b3f0989c8a8d56c9874f7c5d08fadf1c407c
Crowdbotics <team@crowdbotics.com>
2023-07-06 01:56:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "still-pond-41931",
+  url: "https://still-pond-41931.botics.co",
+  api: "https://still-pond-41931.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
